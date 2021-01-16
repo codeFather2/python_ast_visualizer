db3013414b973a3058f16f64e1e2cb139ec77990
Dmitry Litovchenko <Dmitry.Litovchenko1@yandex.ru>
2021-01-16 18:04:31 +0300

    Modify visualizer (WIP)

--- app.py
+++ app.py
@@ -1,23 +1,25 @@
 from parser_ import Parser
 from lexer import Tokenizer
 from termcolor import colored
-from logging import getLogger
-from visualizer import Visualizer
+from logging import getLogger, log
+from visualizer import Visualizer, VisualizingMode as vis_mode
 from logger import create_logger
 
 def main():
     print('AST builder/visualizer is currently under development...')
-    filename = 'testdata/parser_test_loops.py'
-    test_file = open(filename, 'r')
+    filename = 'testdata/parser_test_example.py'
+    test_file = open(filename, 'r').read()
     logger =  create_logger()
-    tokenizer = Tokenizer(test_file.read(), logger)
+    tokenizer = Tokenizer(test_file, logger)
     tokens, error = tokenizer.tokenize()
     if error :
-        print(colored(error, 'red'))
+        logger.error(error.__repr__)
+    logger.info('Tokenization is finished.')
     parser = Parser(tokens, logger)
     result = parser.parse()
-    print(result)
-    Visualizer(result, filename).visualize()
+    logger.info('Parsing is finished.')
+    Visualizer(result, filename, test_file).visualize(vis_mode.AST)    
+    logger.info('Visualization is finished.')
 
 if __name__ == "__main__":
     main()
--- visualizer.py
+++ visualizer.py
@@ -1,27 +1,84 @@
+from io import TextIOWrapper
 import graphviz as g
 import nodes
-from typing import List
+from typing import List, Tuple
+from enum import Enum
+from lexer_utils import TokenType
+
+class VisualizingMode(Enum):
+    AST = 0
+    EXECUTION = 1
 
 class Visualizer:
 
-    def __init__(self, root : nodes.Root, file_name: str) -> None:
+    def __init__(self, root : nodes.Root, file_name: str, source_code : str) -> None:
         self.root = root
         self.file_name = file_name
         self.id = 0
         self.graph = g.Digraph(f"Visualizing of {file_name}")
+        self.source_code = source_code
 
-    def visualize(self):
-        self.create_subtree(self.root)
-        self.graph.render('output/output')
+    def visualize(self, mode : VisualizingMode):
+        self.id = 0
+        self.graph = g.Digraph(f"Visualizing of {self.file_name}")
+
+        if mode == VisualizingMode.AST:
+            self.visualize_ast('Root', self.root)
+        elif mode == VisualizingMode.EXECUTION:
+            self.visualize_execution('Entry', self.root)
+        self.graph.render(f'output/output{mode}')
     
-    def create_subtree(self, node: nodes.Node) -> str:
+    def visualize_ast(self, node_name: str, node: nodes.Node) ->  str:
         key = str(self.id)
-        value = node.value if hasattr(node, 'value')  else ' '.join(map(lambda x : x.__str__(), node.children))
-        self.graph.node(key, f'{node.__class__.__name__}\n {value}')
+        node_name = node_name.capitalize() if len(node_name) > 0 else node.__class__.__name__
+        self.graph.node(key, f'{node_name}\n\n{self.get_text_for_node(node)}')
         self.id += 1
-        if hasattr(node, 'children'):
+        fields_to_exclude = ['children', 'value', 'span']
+        children = list(filter(lambda x: x[0] not in fields_to_exclude, node.__dict__.items()))
+        
+        if len(children) == 0 and hasattr(node, 'children'):
             for child in node.children:
-                child_key = self.create_subtree(child)
+                child_key = self.visualize_ast('', child)
+                self.graph.edge(key, child_key)
+        else:
+            for name, child in children:
+                if child is None:
+                    continue
+                child_key = self.visualize_ast(name, child)
                 self.graph.edge(key, child_key)
         return key
 
+    def visualize_execution(self, node_name: str, node: nodes.Node) ->  str:
+        key = str(self.id)
+        node_name = node_name.capitalize() if len(node_name) > 0 else node.__class__.__name__
+        self.graph.node(key, f'{node_name}\n\n{self.get_text_for_node(node)}')
+        self.id += 1
+        fields_to_exclude = ['children', 'value', 'span']
+        children = list(filter(lambda x: x[0] not in fields_to_exclude, node.__dict__.items()))
+        child_keys: List[str] = []
+        if len(children) == 0 and hasattr(node, 'children'):
+            for child in node.children:
+                child_keys.append(self.visualize_execution('', child))
+        else:
+            for name, child in children:
+                if child is None:
+                    continue
+                child_keys.append(self.visualize_execution(name, child))
+
+        if len(child_keys) > 0:
+            self.graph.edge(key, child_keys[0])
+            for index in range(1, len(child_keys)):
+                current = child_keys[index]
+                prev = child_keys[index - 1]
+                self.graph.edge(prev, current)
+        return key
+
+    def get_text_for_node(self, node: nodes.Node) -> str:
+        if isinstance(node, TokenType):
+            return str(node)
+        if isinstance(node, nodes.Root):
+            return ''
+        if issubclass(type(node), nodes.Terminal):
+            return node.value
+        else:
+            return self.source_code[node.span.begin: node.span.end]
