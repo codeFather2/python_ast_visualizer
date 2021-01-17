7024070a6c17356147f72c0e8c8add2820ceb060
Dmitry Litovchenko <Dmitry.Litovchenko1@yandex.ru>
2021-01-18 00:45:35 +0300

    Execution vizualizing last try

--- visualizer.py
+++ visualizer.py
@@ -10,6 +10,18 @@ class VisualizingMode(Enum):
     AST = 0
     EXECUTION = 1
 
+class SubTree:
+    def __init__(self, key, children, is_branching) -> None:
+        self.key = key
+        self.children = children
+        self.branching = is_branching
+    
+    def __str__(self) -> str:
+        return f'{str(self.keys)} {self.branching}'
+    
+    def __repr__(self) -> str:
+        return self.__str__()
+
 class Visualizer:
 
     def __init__(self, root : nodes.Root, file_name: str, source_code : str, output_file : str, logger : Logger) -> None:
@@ -21,7 +33,7 @@ class Visualizer:
         self.graph = g.Digraph(f"Visualizing of {file_name}")
         self.source_code = source_code
         self.fields_to_exclude = ['children', 'value', 'span', 'wrapped_tokens']
-        self.parents = []
+        self.all_keys = []
 
     def visualize(self, mode : VisualizingMode):
         self.id = 0
@@ -38,7 +50,7 @@ class Visualizer:
     def visualize_ast(self, name: str, node: nodes.Node) ->  str:
         key = self.add_node(name, node)
         children = self.get_children(node)
-        
+
         if isinstance(node, nodes.WrapperNode):
             return key
 
@@ -54,102 +66,102 @@ class Visualizer:
                 self.graph.edge(key, child_key)
         return key
 
-    def visualize_execution(self, name: str, node: nodes.Node) ->  List[str]:
-        keys = [self.add_node(name, node)]
-        self.parents.append(keys[0])
+    def visualize_execution(self, name: str, node: nodes.Node) -> SubTree:
+        key = self.add_node(name, node)
         children = self.get_children(node)
 
         if isinstance(node, nodes.WrapperNode) or issubclass(node.__class__, nodes.Expression):
-            return keys[0]
+            return SubTree(key, [], False)
 
-        children_keys: List[str] = []
+        sub_trees: List[SubTree] = []
         if len(children) == 0 and hasattr(node, 'children'):
             for child in node.children:
-                self.handle_child_execution(children_keys, child, '')
+                self.handle_child_execution(sub_trees, child, '')
         else:
             for name, child in children:
-                self.handle_child_execution(children_keys, child, name)
+                self.handle_child_execution(sub_trees, child, name)
 
-        if len(children_keys) > 0:
-            first = children_keys[0]
-            while isinstance(first, list):
-                first = first[0]
-            self.add_tails_to_head(keys, first)
-            self.add_edges_by_list(children_keys)
-        self.parents.pop()
-        return [keys, children_keys[-1]]
+        if len(sub_trees) > 0:
+            first = sub_trees[0]
+            while first.key == None:
+                first = first.children[0]
+            self.graph.edge(key, first.key)
+            self.add_edges_by_list(sub_trees)
+        return SubTree(None, sub_trees, False)
 
-    def handle_child_execution(self, keys, child, name):
+    def handle_child_execution(self, sub_trees, child, name):
         if child is None:
             return
-        if isinstance(child, nodes.BlockStatement):
-            keys.append(self.prepare_block_execution(child))
+        elif isinstance(child, nodes.BlockStatement):
+            st_keys = []
+            for st in child.children:
+                st_keys.append(self.visualize_execution('', st))
+            self.add_edges_by_list(st_keys)
+            sub_trees.append(SubTree(None, st_keys, False))
         elif isinstance(child, nodes.IfElseStatement):
-            keys.append(self.prepare_if_execution(child))
+            sub_trees.append(self.prepare_if_execution(child))
         else:
-            keys.append(self.visualize_execution(name, child))
+            sub_trees.append(self.visualize_execution(name, child))
 
     def prepare_if_execution(self, node : nodes.IfElseStatement):
         keys = []
         condition = self.visualize_execution('If', node.condition)
         keys.append(condition)
-
+        branches = []
         true_b = None
         if isinstance(node.true_branch, nodes.BlockStatement):
-            block_keys = self.prepare_block_execution(node.true_branch)[0]
-            self.graph.edge(condition, block_keys[0])
-            true_b = block_keys[-1]
+            st_keys = []
+            for st in node.children:
+                st_keys.append(self.visualize_execution('', st))
+            true_b = SubTree(None, st_keys, False)
         else:
             true_b = self.visualize_execution('True', node.true_branch)
 
+        branches.append(true_b)
         false_b = None
-        if node.false_branch and isinstance(node.false_branch, nodes.BlockStatement):
-            block_keys = self.prepare_block_execution(node.false_branch)[0]
-            self.graph.edge(condition, block_keys[0])
-            false_b = block_keys[-1]
-        elif node.false_branch and isinstance(node.false_branch, nodes.IfElseStatement):
-            false_b = self.prepare_if_execution(node.false_branch)[0]
-            self.graph.edge(condition, false_b[0])
-            false_b = false_b[-1]
+        if node.false_branch and isinstance(node.false_branch, nodes.IfElseStatement):
+            false_b = self.prepare_if_execution(node.false_branch)
+            self.graph.edge(condition.key, false_b.key)
+        elif isinstance(node.false_branch, nodes.BlockStatement):
+            st_keys = []
+            for st in node.children:
+                st_keys.append(self.visualize_execution('', st))
+            false_b = SubTree(None, st_keys, False)
         else:
             false_b = self.visualize_execution('False', node.false_branch) if node.false_branch else None
 
-        self.graph.edge(self.parents[-1], condition)
         if false_b:
-            return [true_b, false_b]
-        else:
-            return true_b
+            branches.append(false_b)
+        condition.children = branches
+        condition.branching = True
+        return condition
 
-    def prepare_block_execution(self, node : nodes.BlockStatement):
-        keys : List[str] = []
-        for st in node.children:
-            if isinstance(st, nodes.IfElseStatement):
-                keys.append(self.prepare_if_execution(st))
-            else:
-                keys.append(self.visualize_execution('', st))
-        self.add_edges_by_list(keys)
-        if len(keys) == 2:
-            self.graph.edge(self.parents[-1], keys[0])
-            return keys[-1]
-
-        return keys
-
-    
-    
-    def add_edges_by_list(self, keys):
-
+    def add_edges_by_list(self, keys : List[SubTree]):
         for index in range(1, len(keys)):
-                current = keys[index]
-                prev = keys[index - 1]
-                if isinstance(current, list):
-                    self.add_tail_to_heads(prev, current)
-                elif isinstance(prev, list):
-                    self.add_tails_to_head(prev, current)
-                else:
-                    self.graph.edge(prev, current)
+            current = keys[index]
+            prev = keys[index - 1]
+            self.add_edge(prev, current)
+
+    def add_edge(self, prev: SubTree, current: SubTree):
+        curr_len = len(current.children)
+        prev_len = len(prev.children)
+        if curr_len == 0 and prev_len == 0:
+            self.graph.edge(prev.key, current.key)
+        elif not current.key:
+            self.add_edge(prev, current.children[0])
+        elif not prev.key:
+            self.add_edge(prev.children[-1], current)
+        elif prev.branching:
+            for prev_child in prev.children:
+                self.add_edge(prev_child, current)
+        elif current.branching:
+            for curr_child in current.children:
+                self.add_edge(prev, curr_child)
+        else:
+            self.graph.edge(prev.key, current.key)
 
     def add_tail_to_heads(self, tail, heads):
-        for head in heads:
+        for head in heads.children:
             if isinstance(head, list):
                     continue
             elif isinstance(tail, list):
